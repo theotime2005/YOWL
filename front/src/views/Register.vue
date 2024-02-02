0764d8d0eda5cb953b55bcb4b0fa9510814479f9
theotime2005 <theotime.berthod@epitech.digital>
2024-02-02 11:13:49 +0100

    Page register pour le front

---
+++ front/src/views/Register.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      email: "",
+      password: "",
+      is_visible: false,
+      passwordType: "password" // Ajout d'une variable pour stocker le type de mot de passe
+    }
+  },
+  methods: {
+    toggle_hide_password() {
+      this.is_visible = !this.is_visible;
+      this.passwordType = this.is_visible ? "text" : "password";
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>Créer un compte</h1>
+  <p>Renseignez les champs suivants:</p>
+  <form @submit.prevent>
+    <label for="username">Nom d'utilisateur</label>
+    <input type="text" id="username" class="editBox" v-model="username" required>
+    <label for="email">Email</label>
+    <input type="email" class="editBox" id="email" v-model="email" required>
+    <label for="password">Mot de passe</label>
+    <input :type="passwordType" class="editBox" id="password" v-model="password" required>
+    <button @click="toggle_hide_password">
+      {{ is_visible ? 'Masquer' : 'Afficher' }} le mot de passe
+    </button>
+    <p>Votre mot de passe doit comporter au moins 10 caractères dont une majuscule, une minuscule, un chiffre et un symbole.</p>
+  </form>
+</template>
+
+<style scoped>
+
+</style>
---
+++ front/src/views/Welcome.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+  <h1>Bienvenu sur le site Pets</h1>
+  <p>Pour utiliser le site, connectez-vous ou créez un compte.</p>
+  <button @click="this.$router.push('/register')">Créer un compte</button>
+</template>
+
+<style scoped>
+
+</style>
